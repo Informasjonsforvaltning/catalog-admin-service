ff41b650cf4545378790b09455349c464d7493a7
Hege Aalvik <hegeaal@gmail.com>
2023-06-01 10:42:05 +0200

    feat: get codelist by id

--- src/main/kotlin/no/digdir/catalog_admin_service/service/CodeListService.kt
+++ src/main/kotlin/no/digdir/catalog_admin_service/service/CodeListService.kt
@@ -1,11 +1,16 @@
 package no.digdir.catalog_admin_service.service
 
+import no.digdir.catalog_admin_service.model.CodeList
 import no.digdir.catalog_admin_service.model.CodeLists
 import no.digdir.catalog_admin_service.repository.CodeListRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class CodeListService(private val codeListRepository: CodeListRepository) {
     fun getCodeLists(): CodeLists =
         CodeLists(codeLists = codeListRepository.findAll())
+
+    fun getCodeListById(codeListId: String): CodeList? =
+        codeListRepository.findByIdOrNull(codeListId)
 }
