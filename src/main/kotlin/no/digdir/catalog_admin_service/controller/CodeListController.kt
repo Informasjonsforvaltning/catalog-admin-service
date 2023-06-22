4e567dde9d445d105d2bc6064e609da57b3b4dfe
Hege Aalvik <hegeaal@gmail.com>
2023-06-22 12:49:38 +0200

    chore¨: add logger to codelist create

--- src/main/kotlin/no/digdir/catalog_admin_service/controller/CodeListController.kt
+++ src/main/kotlin/no/digdir/catalog_admin_service/controller/CodeListController.kt
@@ -6,6 +6,7 @@ import no.digdir.catalog_admin_service.model.CodeLists
 import no.digdir.catalog_admin_service.model.JsonPatchOperation
 import no.digdir.catalog_admin_service.security.EndpointPermissions
 import no.digdir.catalog_admin_service.service.CodeListService
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -21,6 +22,8 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+
+private val logger = LoggerFactory.getLogger(CodeListController::class.java)
 
 @Controller
 @CrossOrigin
@@ -42,7 +45,7 @@ open class CodeListController(
 
 
     @GetMapping(value = ["/{codeListId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun getCatalogById(
+    fun getCodeListById(
         @AuthenticationPrincipal jwt: Jwt,
         @PathVariable catalogId: String,
         @PathVariable codeListId: String
@@ -92,11 +95,13 @@ open class CodeListController(
         @RequestBody newCodeList: CodeListToBeCreated
     ): ResponseEntity<Unit> =
         if (endpointPermissions.hasOrgAdminPermission(jwt, catalogId)) {
+            logger.info("creating codelist for ${catalogId}")
             val created = codeListService.createCodeList(newCodeList, catalogId)
             ResponseEntity(
                 locationHeaderForCreated(newId = created.id, catalogId),
                 HttpStatus.CREATED
             )
+
         } else ResponseEntity<Unit>(HttpStatus.FORBIDDEN)
 }
 
